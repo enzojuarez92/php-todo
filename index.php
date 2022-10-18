f5c3346423f3b48dee05cfd84524325f15723ca3
EnzoProgramer <enzodanielj@gmail.com>
2022-10-17 22:42:26 -0300

    todolist finish

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>TODO PHP</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="styles/styles.css" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
@@ -14,12 +14,12 @@
 </head>
 
 <body>
-    <div class="container text-center my-3">
-        <h1 class="mb-3">Listado de Tareas</h1>
+    <div class="container text-center">
+        <h1 class="my-5 text-primary">Listado de Tareas</h1>
         <div class="row">
             <div class="col-6">
-                <table class="table">
-                    <thead>
+                <table class="table table-dark table-striped">
+                    <thead class="">
                         <tr>
                             <td scope="col">TAREA</td>
                             <td scope="col">DESCRIPCION</td>
@@ -34,8 +34,15 @@
             </div>
             <div class="col-6">
                 <?php if (!$_GET) : ?>
-                    <form class="text-start" action="./controllers/create.php" method="POST">
+                    <form class="text-start p-4 border border-1 border-info rounded-3" action="./create.php" method="POST">
+                        <h2 class="text-center text-primary">Nueva Tarea</h2>
                         <?php include_once './views/create-task.php' ?>
+                    </form>
+                <?php endif ?>
+                <?php if ($_GET) : ?>
+                    <form class="text-start p-4 border border-1 border-dark rounded-3" action="./update.php" method="GET">
+                        <h2 class="text-center text-success">Editar Tarea</h2>
+                        <?php include_once './views/update-task.php' ?>
                     </form>
                 <?php endif ?>
             </div>
